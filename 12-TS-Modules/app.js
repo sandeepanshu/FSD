e28968a0c3615ae9a0ef8ca05f0cfdccfaa7c283
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-17 21:33:47 +0530

    Add TypeScript implementation for Employee and IEmployee classes with methods for data handling

---
+++ 12-TS-Modules/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Employee_1 = require("./Employee");
+var employee = new Employee_1.Employee("Sandeep", "Sharma", "sandeepks9199@gmail.com", "Male", "Manager");
+employee.bio();
---
+++ 12-TS-Modules/app.ts
@@ -0,0 +1,10 @@
+import { Employee } from "./Employee";
+
+let employee = new Employee(
+  "Sandeep",
+  "Sharma",
+  "sandeepks9199@gmail.com",
+  "Male",
+  "Manager"
+);
+employee.bio();
