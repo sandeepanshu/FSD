2dcbe5de33318ebef3ea2804aebda3cc27f77083
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-17 13:50:56 +0530

    Add initial TypeScript and JavaScript files for course and app title

---
+++ 01-TS-Basics/app.ts
@@ -0,0 +1,14 @@
+let courseName: string = "TS Fundamentals";
+console.log(courseName);
+
+/*
+    1. starts with a lowercase, only class , interface , enum will starts with uppercase
+    2. use camel case for more words in variables
+    3. don't start with a number
+    4. a-z , A-Z , 0-9 , _ , $
+    5*. max chars length is 15
+    6. for const use ALL_CAPS
+ */
+
+const APP_TITLE: string = "FaceBook Clone";
+console.log(APP_TITLE);
