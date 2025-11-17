921f5bc46c54d29618a2453c1a5acca47eae3a90
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-17 15:12:50 +0530

    Refactor comments for clarity and consistency in app.js

--- 08-TS-ES6-Features/app.js
+++ 08-TS-ES6-Features/app.js
@@ -1,23 +1,23 @@
 /*
     1. Template Strings (`)
     2. Optional Parameters
-    3. let , const
+    3. let, const
     4. for-of loop
     5. destructuring
     6. Spread Operator
     7. Arrow Function
  */
 // 1. Template Strings (`)
-var employeeName = 'John';
+var employeeName = "John";
 var employeeAge = 45;
 console.log("NAME : " + employeeName + " AGE : " + employeeAge);
-console.log("NAME : " + employeeName + " AGE : " + employeeAge);
+console.log("NAME : ".concat(employeeName, " AGE : ").concat(employeeAge));
 // 2. Optional Parameters
 var printNumbers = function (start, end) {
     if (end === void 0) { end = 50; }
-    var result = '';
+    var result = "";
     for (var i = start; i <= end; i++) {
-        result += i + " ";
+        result += "".concat(i, " ");
     }
     console.log(result);
 };
