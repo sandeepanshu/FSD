9a24db537846ff5704eeac645d444a63446a2e00
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-17 23:06:21 +0530

    React Started

---
+++ first-app/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Message from './components/Message'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+      <h1>Welcome to React JS</h1>
+        <Message/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ first-app/src/components/Message.tsx
@@ -0,0 +1,11 @@
+import './Message.css';
+
+const Message = () => {
+  return (
+    <>
+      <h1 className="colors">Message component</h1>
+    </>
+  );
+};
+
+export default Message;
