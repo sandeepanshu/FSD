5f8653737fd3a60463c16510ff16b021cbdf49af
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-18 00:06:36 +0530

    Bootstrap added

---
+++ 02-React-JS-Core-Module-FINAL/03-React-JS-Bootstrap-Styles/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./components/card/Card";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+        <a href="/">React with Bootstrap</a>
+      </nav>
+      <Card />
+    </>
+  );
+}
+
+export default App;
