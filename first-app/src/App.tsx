3c76beab49d06007a66f1337011f5341cd01e38e
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-18 12:12:53 +0530

    Forms Handleling complete

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,18 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
-import Registration from "./components/Registration";
-import SMSApp from "./components/SMSApp";
-import UserInput from "./components/UserInput";
 
 function App() {
   return (
     <>
       <NavBar />
       {/* Main Content */}
-      <UserInput />
-      <SMSApp />
-      <Registration />
+
       <Footer />
     </>
   );
