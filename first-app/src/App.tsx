b775ef6b6664f2a87eee4b7c10a9bb694e61f7a8
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-18 11:51:24 +0530

    Shopping Cart updates

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
-import ProductItem from "./components/ProductItem";
 
 function App() {
   return (
     <>
       <NavBar />
-      {/* <Welcome /> */}
-      {/* <WishMessage /> */}
-      <ProductItem />
+      {/* Main Content */}
       <Footer />
     </>
   );
