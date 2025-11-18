b399ab6fe77c67daef6c897a8f3ed87bc1ff9fdb
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-11-18 11:45:59 +0530

    File seperated for event binding

---
+++ 02-React-JS-Core-Module-FINAL/05-React-JS-Events-Bindings/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import ProductItem from "./components/ProductItem";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      {/* <Welcome /> */}
+      {/* <WishMessage /> */}
+      <ProductItem />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
