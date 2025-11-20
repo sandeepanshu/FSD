1f345e780ac07a5876e043750da9190ba4e43e4a
sandeepanshu <57533185+sandeepanshu@users.noreply.github.com>
2025-11-21 00:35:58 +0530

    Custom module add

--- 03-Server-Side-Development/04-NODE-Custom-Modules/server.ts
+++ 03-Server-Side-Development/04-NODE-Custom-Modules/server.ts
@@ -1,4 +1,5 @@
 import http from 'http'; // load modules
+import * as str from "./util/strOperations.ts";
 
 const hostName:string = '127.0.0.1';
 const port:number = 5000;
@@ -8,7 +9,13 @@ const server:http.Server = http.createServer((request:http.IncomingMessage , res
     response.statusCode = 200;
     response.setHeader('Content-Type' , 'text/html');
 
-    
+    // custom Modules
+    let message:string = 'Good Morning';
+    let rev:string = str.reverseStr(message);
+    print(rev);
+
+    let sum = str.add(10,200);
+    print(sum.toString());
 
     response.end(`<pre style="color: orangered">${output}</pre>`);
 });
