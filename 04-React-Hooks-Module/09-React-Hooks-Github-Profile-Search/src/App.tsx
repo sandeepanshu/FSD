6b627f74c1a52e5c4ab8bcf274c39a17bce61a01
sandeepanshu <57533185+sandeepanshu@users.noreply.github.com>
2025-11-21 16:53:02 +0530

    Code added for github profile search with hooks

--- 04-React-Hooks-Module/09-React-Hooks-Github-Profile-Search/src/App.tsx
+++ 04-React-Hooks-Module/09-React-Hooks-Github-Profile-Search/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import GithubProfileSearchApp from "./components/github/GithubProfileSearchApp";
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
+        <GithubProfileSearchApp />
         <Routes></Routes>
       </Router>
     </>
