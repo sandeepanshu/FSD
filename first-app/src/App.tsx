3092db7ecd0ae0e549e10fc9c16fd0365457d9bc
sandeepanshu <57533185+sandeepanshu@users.noreply.github.com>
2025-11-21 14:15:05 +0530

    React-Hooks-Conditional-Looping added

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import AuthUser from "./components/AuthUser";
+import Customers from "./components/Customers";
+import EmployeeCards from "./components/EmployeeCards";
+import HobbySelector from "./components/HobbySelector";
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
+        <AuthUser />
+        <Customers />
+        <EmployeeCards />
+        <HobbySelector />
         <Routes>
 
         </Routes>
