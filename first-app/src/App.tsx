6ba57b95b968860a2af9842eb0626257a3682fdf
sandeepanshu <57533185+sandeepanshu@users.noreply.github.com>
2025-11-21 15:40:19 +0530

    Code Clean

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,65 +1,14 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import DigitalWatch from "./components/life-cycle/DigitalWatch";
-import ComponentA from "./components/props-drilling/ComponentA";
-import { useState } from "react";
-import type { UserInfo } from "./components/props-drilling/UserInfo";
-import UserContext from "./components/context-api/UserContext";
-import ComponentOne from "./components/context-api/ComponentOne";
-interface IState {
-  userInfo: UserInfo;
-}
+ 
 function App() {
-  const [appState, setAppState] = useState<IState>({
-    userInfo: {
-      author: "SANDEEP SHARMA",
-      email: "sandeepks9199@gmail.com",
-    },
-  });
+  
 
   return (
     <>
       <Router>
         <NavBar />
-        {/* Props Drilling Concept */}
-        <section className="mt-3">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <div className="card">
-                  <div className="card-body bg-success text-white">
-                    <p className="h4">App Component</p>
-                    <small>{JSON.stringify(appState.userInfo)}</small>
-                    <ComponentA userInfo={appState.userInfo} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        {/* Context API */}
-        <section className="mt-3">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <div className="card">
-                  <div className="card-body bg-success text-white">
-                    <p className="h4">App Component</p>
-                    <small>{JSON.stringify(appState.userInfo)}</small>
-
-                    <UserContext.Provider value={appState.userInfo}>
-                      <ComponentOne />
-                    </UserContext.Provider>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        {/* Life Cycle */}
-        <DigitalWatch />
+         
 
         <Routes></Routes>
       </Router>
--- first-app/src/components/props-drilling/UserInfo.ts
+++
@@ -1,4 +0,0 @@
-export interface UserInfo {
-    author : string;
-    email : string;
-}
