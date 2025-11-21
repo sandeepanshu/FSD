d7e3e0f2c670ea4d2f2bdec9453f046296757380
sandeepanshu <57533185+sandeepanshu@users.noreply.github.com>
2025-11-21 13:55:03 +0530

    App Clean

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Registration from "./components/register/Registration";
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
-        {/* <UserInput /> */}
-        {/* <SMSApp /> */}
-        <Registration />
         <Routes>
 
         </Routes>
